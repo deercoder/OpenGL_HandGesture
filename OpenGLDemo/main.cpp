fce2ce570e6a4e6a837aa6b63d559d63a91834ba
Chang Liu <lllccc2009@gmail.com>
2016-08-16 12:58:02 -0400

    Initial Commit

---
+++ OpenGLDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by Chang Liu on 8/16/16.
+//  Copyright © 2016 Chang Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
